c5b504caf24eb868bb5424707fcf7e797f22f3f7
aolman <aolman@umich.edu>
2024-09-28 19:26:13 -0400

    updated get response

--- get_response.py
+++ get_response.py
@@ -11,8 +11,9 @@ system_prompt = {
     "role": "system",
     "content":
     "You are a helpful assistant. You reply concisely, and you only reply if \
-    the prompt is related to recycling. If not, then you redirect the \
-    conversation to recycling."
+    the prompt is related to recycling. If not, then kindly redirect the \
+    conversation to recycling. Try to use less than 40 words and no numbered \
+    lists."
 }
 
 chat_history = [system_prompt]
@@ -25,7 +26,7 @@ while True:
     response = client.chat.completions.create(model="llama3-70b-8192",
     messages = chat_history,
     max_tokens = 100,
-    temperature = 0.4)
+    temperature = 0.2)
     chat_history.append({"role": "assistant", \
     "content": response.choices[0].message.content})
     
