4aaecfced199dd976218a23f83e78ed2d054286c
aolman <aolman@umich.edu>
2024-09-28 19:04:02 -0400

    LLM script added

---
+++ get_response.py
@@ -0,0 +1,33 @@
+import os
+
+from groq import Groq
+
+client = Groq(
+    api_key = os.environ.get("GROQ_KEY")
+)
+
+
+system_prompt = {
+    "role": "system",
+    "content":
+    "You are a helpful assistant. You reply concisely, and you only reply if \
+    the prompt is related to recycling. If not, then you redirect the \
+    conversation to recycling."
+}
+
+chat_history = [system_prompt]
+
+while True:
+    
+    user_input = input("You: ")
+    chat_history.append({"role": "user", "content": user_input})
+
+    response = client.chat.completions.create(model="llama3-70b-8192",
+    messages = chat_history,
+    max_tokens = 100,
+    temperature = 0.4)
+    chat_history.append({"role": "assistant", \
+    "content": response.choices[0].message.content})
+    
+    print("Assistant: ", response.choices[0].message.content)
+
